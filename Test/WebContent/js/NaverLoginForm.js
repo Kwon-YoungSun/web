ef5f912167d916828cc671a6f1b5976ef1ee9a25
class02 <class02@host.docker.internal>
2020-10-22 18:22:54 +0900

    day1022 last commit

--- Test/WebContent/js/NaverLoginForm.js
+++ Test/WebContent/js/NaverLoginForm.js
@@ -8,14 +8,39 @@ document.getElementById('months').innerHTML = str;
 
 // 경우의 수
 /*
-	1. 필수 정보입니다.(focusout)
+	1. 필수 정보입니다.(focusout) --> 입력한 데이터가 없이 포커스아웃한 경우
 	2. 이미 사용중이거나 탈퇴한 아이디입니다. 혹은 ~~ 한 
-	형식에 맞지 않습니다.
-	3. 멋진 아이디네요!
-	4. 비밀번호 검사의 경우 > 비밀번호가 일치하지 않습니다.
+	형식에 맞지 않습니다. --> 입력한 데이터가 있을 때 형식검사에 맞지 않는 경우
+	3. 멋진 아이디네요! --> 입력한 데이터가 있고 형식검사에 맞는 경우
+	4. 비밀번호 검사의 경우 > 비밀번호가 일치하지 않습니다. 
 */
 
-function func01(){
-	document.getElementById('id_confirm').style.display = 'block';
-	alert('포커스아웃');
-}
+$(document).ready(function(){
+	var str = ''; 	// 표시할 메세지를 담을 변수
+	var pat = /^[a-z]{1}[a-z0-9_-]{4,18}$/g;
+	var result;
+	var data;
+	
+	// 포커스아웃할 경우
+	$('#id').focusout(function(){	
+		// 입력한 데이터를 가져오기
+		data = $('#id').val();
+		
+		// 형식검사하기
+		result = pat.test(data);
+		
+		if(data == '') result = true;
+		
+		// 결과를 출력한다.
+		if(result == true) {
+			if(data) {
+				str = '멋진 아이디네요!';
+			} else {
+				str = '필수 정보입니다.';
+			}
+		} else {
+			str = '잘못된 형식입니다.';
+		}
+		$('#id_confirm').text(str).css('display', 'block');		
+	});
+});
--- webme/WebContent/js/jq01/Ex01.js
+++ webme/WebContent/js/jq01/Ex01.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
-		$('.w3-button').click(function(){
-			// 내용 읽어오고
-			var sname = $(this).siblings().first().text();
-			var sage = $(this).siblings().eq(1).text();
-			var stel = $(this).siblings().eq(2).text();
-			var smail = $(this).siblings().eq(3).text();
-			var sgen = $(this).siblings().eq(4).text();
-			
-			// sample.html에 전달하고
-			$('#name').val(sname);
-			$('#age').val(sage);
-			$('#tel').val(stel);
-			$('#mail').val(smail);
-			$('#gen').val(sgen);
-			
-			// submit
-			$('#frm1').submit();
-		});
+	$('.w3-button').click(function(){
+		// 내용 읽어오고
+		var sname = $(this).siblings().first().text();
+		var sage = $(this).siblings().eq(1).text();
+		var stel = $(this).siblings().eq(2).text();
+		var smail = $(this).siblings().eq(3).text();
+		var sgen = $(this).siblings().eq(4).text();
+		
+		// sample.html에 전달하고
+		$('#name').val(sname);
+		$('#age').val(sage);
+		$('#tel').val(stel);
+		$('#mail').val(smail);
+		$('#gen').val(sgen);
+		
+		// submit
+		$('#frm1').submit();
 	});
+	
+	$('#header').click(function(){
+		$('.data').removeClass("w3-hide");
+	});
+});
